a32a470743ffa236fa02934c25a93e21196c99cd
Bryan Ngo <48371418+bdngo@users.noreply.github.com>
2022-12-17 00:04:48 -0800

    potential merge conflict incoming

--- src/fishbot/src/cv.py
+++ src/fishbot/src/cv.py
@@ -6,31 +6,6 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 
-NUM_CLUSTERS = 2
-
-def process_image(img: Image) -> None:
-    pub = rospy.Publisher("/image_segmented", Image, queue_size=10)
-    r = rospy.Rate(10)
-
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
-
-    # Downsample img by a factor of 2 first using the mean to speed up K-means
-    img_d = cv2.resize(src=cv_image, dsize=(cv_image.shape[1]//2, cv_image.shape[0]//2), interpolation=cv2.INTER_NEAREST)
-
-    hsvFrame = cv2.cvtColor(img_d, cv2.COLOR_BGR2HSV)
-
-    blue_lower = np.array([94, 80, 2], np.uint8)
-    blue_upper = np.array([120, 255, 255], np.uint8)
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-
-    # Upsample the image back to the original image (img) using nearest interpolation
-    img_u = cv2.resize(src=blue_mask, dsize=(cv_image.shape[1], cv_image.shape[0]), interpolation=cv2.INTER_NEAREST)
-    
-    img_u.astype(np.uint8)
-
-
-
 def vision() -> None:
     """
     Caller for image processing callback.
@@ -38,13 +13,49 @@ def vision() -> None:
     rospy.Subscriber("/usb_cam/image_raw", Image, process_image)
     rospy.spin()
 
+
 def main() -> None:
     """
     Main driver for computer vision node.
     """
-    rospy.init_node("computer_vision", anonymous=True)
+    # rospy.init_node("computer_vision", anonymous=True)
 
-    vision()
+    # vision()
+
+    cv_image = cv2.imread("cv_image.png")
+
+    # downsample cv_image by a factor of 2 using nearest interpolation
+    img_d = cv2.resize(src=cv_image, dsize=(cv_image.shape[1]//2, cv_image.shape[0]//2), interpolation=cv2.INTER_NEAREST)
+    
+    hsvFrame = cv2.cvtColor(img_d, cv2.COLOR_BGR2HSV)
+
+    # define BGR lower and upper limits to threshold blue color
+    blue_lower = np.array([100, 80, 10], np.uint8)
+    blue_upper = np.array([120, 255, 255], np.uint8)
+
+    # creates a mask
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+
+    # creates a kernel used to remove from mask
+    kernel = np.ones((7, 7), np.uint8)
+
+    # remove noise from the mask
+    blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_CLOSE, kernel)
+    blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
+
+    # creates thresholded blue image using the mask
+    thresholded_img = cv2.bitwise_and(img_d, img_d, mask=blue_mask)
+
+    # upsample the image back to the original cv_image by a factor of 2 using nearest interpolation
+    img_u = cv2.resize(src=thresholded_img, dsize=(cv_image.shape[1], cv_image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    img_u = img_u.astype(np.uint8)
+
+
+    # show thresholded image
+    cv2.imshow("image", img_u)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
 
 
 if __name__ == "__main__":
